a299b326ab147cc7ef277907d92974804589a6eb
Александр Зайцев <sdjgsinhzoghaodg@yandex.ru>
2025-08-28 14:45:38 +0300

    basics refactor

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,20 @@ import './styles/main.scss'
 export function App() {
 
   return (
-    <>
-			
-    </>
+    <main className="container layout">
+	    <form
+		    action=""
+		    className="form"
+	    >
+		    <label htmlFor="email">Ваш Email
+			    <input
+			    className="input"
+			    name="email"
+		    
+		      />
+				</label>
+		   
+	    </form>
+    </main>
   )
 }
