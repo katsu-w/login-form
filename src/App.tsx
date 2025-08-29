7c1c222d62fa502c91302b18f53d39ce2539dc9b
Александр Зайцев <sdjgsinhzoghaodg@yandex.ru>
2025-08-29 14:31:38 +0300

    html + css

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,25 @@
-import './styles/main.scss'
+import './styles/main.scss';
 
 export function App() {
-
-  return (
-    <main className="container layout">
-	    <form
-		    action=""
-		    className="form"
-	    >
-		    <label htmlFor="email">Ваш Email
-			    <input
-			    className="input"
-			    name="email"
-		    
-		      />
-				</label>
-		   
-	    </form>
-    </main>
-  )
+	return (
+		<main className="container layout">
+			<form className="form">
+				<div className="fields">
+					<div className="labels">
+						<label htmlFor="email">Ваш Email</label>
+						<label htmlFor="password">Пароль</label>
+						<label htmlFor="password-repeat">Повтор пароля</label>
+					</div>
+					<div className="inputs">
+						<input className="input" name="email" type="email" />
+						<input className="input" name="password" type="password" />
+						<input className="input" name="password-repeat" type="password" />
+					</div>
+				</div>
+				<button type="submit" className="button">
+					Зарегистрироваться
+				</button>
+			</form>
+		</main>
+	);
 }
