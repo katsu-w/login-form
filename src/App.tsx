3c0bf4932e2540e7f6adcaf56f06553dc5846cd6
Александр Зайцев <sdjgsinhzoghaodg@yandex.ru>
2025-09-01 20:50:47 +0300

    sign inputs

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,23 @@
 import './styles/main.scss';
+import { type ChangeEvent, useState } from 'react';
 
 export function App() {
+	const [emailValue, setEmailValue] = useState<string>('');
+	const [passwordValue, setPasswordValue] = useState<string>('');
+	const [passwordRepeatValue, setPasswordRepeatValue] = useState<string>('');
+
+	const onEmailChange: (event: ChangeEvent) => void = ({ target }) => {
+		setEmailValue(target?.value);
+	};
+
+	const onPasswordChange: (event: ChangeEvent) => void = ({ target }) => {
+		setPasswordValue(target?.value);
+	};
+
+	const onPasswordRepeatChange: (event: ChangeEvent) => void = ({ target }) => {
+		setPasswordRepeatValue(target?.value);
+	};
+
 	return (
 		<main className="container layout">
 			<form className="form">
@@ -11,9 +28,27 @@ export function App() {
 						<label htmlFor="password-repeat">Повтор пароля</label>
 					</div>
 					<div className="inputs">
-						<input className="input" name="email" type="email" />
-						<input className="input" name="password" type="password" />
-						<input className="input" name="password-repeat" type="password" />
+						<input
+							className="input"
+							value={emailValue}
+							onChange={onEmailChange}
+							name="email"
+							type="email"
+						/>
+						<input
+							className="input"
+							value={passwordValue}
+							onChange={onPasswordChange}
+							name="password"
+							type="password"
+						/>
+						<input
+							className="input"
+							value={passwordRepeatValue}
+							onChange={onPasswordRepeatChange}
+							name="password-repeat"
+							type="password"
+						/>
 					</div>
 				</div>
 				<button type="submit" className="button">
