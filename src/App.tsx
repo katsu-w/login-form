3566d0d7ee32e5f3722015c12ae7c7505faa9900
Александр Зайцев <sdjgsinhzoghaodg@yandex.ru>
2025-09-03 20:25:02 +0300

    install react hook form + yup packages; started migrating to rhf + yup;

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,63 @@
 import './styles/main.scss';
-import { type ChangeEvent, type FormEvent, useRef, useState } from 'react';
+import { type SubmitHandler, useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+import { useRef } from 'react';
+import type { ObjectSchema } from 'yup';
+
+interface IFormData {
+	email: string;
+	password: string;
+	passwordRepeat: string;
+}
+
+const schema: ObjectSchema<IFormData> = yup
+	.object({
+		email: yup.string().required(),
+		password: yup.string().required(),
+		passwordRepeat: yup.string().required(),
+	})
+	.required();
 
 export function App() {
-	const [emailValue, setEmailValue] = useState<string>('');
-	const [passwordValue, setPasswordValue] = useState<string>('');
-	const [passwordRepeatValue, setPasswordRepeatValue] = useState<string>('');
-	const [error, setError] = useState<null | string>(null);
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm<IFormData>({
+		defaultValues: {
+			email: '',
+			password: '',
+			passwordRepeat: '',
+		},
+		resolver: yupResolver(schema),
+	});
+
+	// const emailValidationProps = {
+	// 	minLength: { value: 8, message: 'Почта должна содержать от 6 символов.' },
+	// 	maxLength: {
+	// 		value: 30,
+	// 		message: 'Имя пользователя должно содержать до 30 символов.',
+	// 	},
+	// 	pattern: {
+	// 		value: /^[a-zA-Z0-9.]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+	// 		message:
+	// 			'Извините, но допускаются только латинские буквы (a-z), цифры (0-9) и точки (.).',
+	// 	},
+	// };
+
+	const emailError = errors.email?.message;
 
 	const submitButton = useRef(null);
 
-	const onEmailChange: (event: ChangeEvent<HTMLInputElement>) => void = ({ target }) => {
-		setEmailValue(target.value);
-	};
+	// 	} else if (passwordValue && passwordValue !== passwordRepeatValue) {
+	// 		setError('Пароли не совпадают.');
 
-	const validate: () => void = () => {
-		if (!/^[a-zA-Z0-9.]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(emailValue)) {
-			setError(
-				'Извините, но допускаются только латинские буквы (a-z), цифры (0-9) и точки (.).',
-			);
-		} else if (emailValue.length > 30) {
-			setError('Имя пользователя должно содержать до 30 символов.');
-		} else if (passwordValue && passwordValue !== passwordRepeatValue) {
-			setError('Пароли не совпадают.');
-		} else {
-			setError(null);
-		}
-	};
-
-	const onPasswordChange: (event: ChangeEvent<HTMLInputElement>) => void = ({
-		target,
-	}) => {
-		setPasswordValue(target.value);
-	};
-
-	const onPasswordRepeatChange: (event: ChangeEvent<HTMLInputElement>) => void = ({
-		target,
-	}) => {
-		setPasswordRepeatValue(target.value);
-	};
-
-	const onSubmit = (event: FormEvent<HTMLFormElement>) => {
-		event.preventDefault();
-		if (emailValue && passwordValue && passwordRepeatValue) {
-			console.log({
-				email: emailValue,
-				password: passwordValue,
-			});
-		}
-	};
+	const onSubmit: SubmitHandler<IFormData> = (formData) => console.log(formData);
 
 	return (
 		<main className="container layout">
-			<form className="form" onSubmit={onSubmit}>
+			<form className="form" onSubmit={handleSubmit((formData) => onSubmit(formData))}>
 				<div className="fields">
 					<div className="labels">
 						<label htmlFor="email">Ваш Email</label>
@@ -61,27 +67,21 @@ export function App() {
 					<div className="inputs">
 						<input
 							className="input"
-							value={emailValue}
-							onChange={onEmailChange}
-							onBlur={validate}
+							{...register('email')}
 							placeholder="example@gmail.com"
 							name="email"
 							type="email"
 						/>
 						<input
 							className="input"
-							value={passwordValue}
-							onChange={onPasswordChange}
-							onBlur={validate}
+							{...register('password')}
 							placeholder="y0uR!pAsS"
 							name="password"
 							type="password"
 						/>
 						<input
 							className="input"
-							value={passwordRepeatValue}
-							onChange={onPasswordRepeatChange}
-							onBlur={validate}
+							{...register('passwordRepeat')}
 							placeholder="y0uR!pAsS"
 							name="password-repeat"
 							type="password"
@@ -89,14 +89,14 @@ export function App() {
 					</div>
 				</div>
 				<button
-					disabled={error !== null}
+					disabled={!!emailError}
 					ref={submitButton}
 					type="submit"
 					className="button"
 				>
 					Зарегистрироваться
 				</button>
-				{error && <p>{error}</p>}
+				{emailError && <p>{emailError}</p>}
 			</form>
 		</main>
 	);
